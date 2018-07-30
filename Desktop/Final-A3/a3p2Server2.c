d1c9999d8de70810fab0323164d78b52b56f03dc
Jack Zumwalt <jzumwalt@dfwmacx0kjg3qn.paloaltonetworks.local>
2018-07-30 11:16:46 -0500

    File commit

---
+++ Desktop/Final-A3/a3p2Server2.c
@@ -0,0 +1,105 @@
+// Jack Zumwalt
+// jsz160130
+// CS 3377.0W1
+// Assignment 3
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <sqlite3.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <time.h>
+
+#define PORTNUMBER 30000
+
+struct serverParm {
+    int connectionDesc;
+};
+
+void *serverThread(void *parmPtr) {
+    
+#define PARMPTR ((struct serverParm *) parmPtr)
+    int recievedMsgLen;
+    char messageBuf[1025];
+    
+    /* Server thread code to deal with message processing */
+    printf("DEBUG: connection made, connectionDesc=%d\n",
+           PARMPTR->connectionDesc);
+    if (PARMPTR->connectionDesc < 0) {
+        printf("Accept failed\n");
+        return(0);    /* Exit thread */
+    }
+    
+    /* Receive messages from sender... */
+    while ((recievedMsgLen=
+            read(PARMPTR->connectionDesc,messageBuf,sizeof(messageBuf)-1)) > 0)
+    {
+        recievedMsgLen[messageBuf] = '\0';
+        printf("Message: %s\n",messageBuf);
+        if (write(PARMPTR->connectionDesc,"GOT IT\n",7) < 0) {
+            perror("Server: write error");
+            return(0);
+        }
+    }
+    close(PARMPTR->connectionDesc);  /* Avoid descriptor leaks */
+    free(PARMPTR);                   /* And memory leaks */
+    return(0);                       /* Exit thread */
+}
+
+main () {
+    int listenDesc;
+    struct sockaddr_in myAddr;
+    struct serverParm *parmPtr;
+    int connectionDesc;
+    pthread_t threadID;
+    
+    /* For testing purposes, make sure process will terminate eventually */
+    alarm(120);  /* Terminate in 120 seconds */
+    
+    /* Create socket from which to read */
+    if ((listenDesc = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("open error on socket");
+        exit(1);
+    }
+    
+    /* Create "name" of socket */
+    myAddr.sin_family = AF_INET;
+    myAddr.sin_addr.s_addr = INADDR_ANY;
+    myAddr.sin_port = htons(PORTNUMBER);
+    
+    if (bind(listenDesc, (struct sockaddr *) &myAddr, sizeof(myAddr)) < 0) {
+        perror("bind error");
+        exit(1);
+    }
+    
+    /* Start accepting connections.... */
+    /* Up to 5 requests for connections can be queued... */
+    listen(listenDesc,5);
+    
+    while (1) /* Do forever */ {
+        /* Wait for a client connection */
+        connectionDesc = accept(listenDesc, NULL, NULL);
+        
+        /* Create a thread to actually handle this client */
+        parmPtr = (struct serverParm *)malloc(sizeof(struct serverParm));
+        parmPtr->connectionDesc = connectionDesc;
+        if (pthread_create(&threadID, NULL, serverThread, (void *)parmPtr)
+            != 0) {
+            perror("Thread create error");
+            close(connectionDesc);
+            close(listenDesc);
+            exit(1);
+        }
+        
+        printf("Parent ready for another connection\n");
+    }
+    
+}
